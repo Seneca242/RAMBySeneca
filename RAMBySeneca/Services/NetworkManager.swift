c87c4b8aab09391be4321898a27e7a809fbf956f
Seneca <flabbergasted242@gmail.com>
2022-04-27 23:52:50 +0300

    made groups structure with files

---
+++ RAMBySeneca/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  RAMBySeneca
+//
+//  Created by Дмитрий Дмитрий on 27.04.2022.
+//
+
+import Foundation
---
+++ RAMBySeneca/Views/CharacterTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CharacterTableViewCell.swift
+//  RAMBySeneca
+//
+//  Created by Дмитрий Дмитрий on 27.04.2022.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
