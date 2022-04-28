1ffb4be0cb3c3894e5d3fe3f44380b08e08d35b9
Seneca <flabbergasted242@gmail.com>
2022-04-28 23:57:53 +0300

    made a model

--- RAMBySeneca/R&MModel/R&MModel.swift
+++ RAMBySeneca/R&MModel/R&MModel.swift
@@ -5,4 +5,55 @@
 //  Created by Дмитрий Дмитрий on 27.04.2022.
 //
 
-import Foundation
+struct RickAndMorty: Decodable {
+    let info: Info?
+    let results: [Characters]?
+}
+
+struct Info: Decodable {
+    let pages: Int?
+    let next: String?
+    let prev: String?
+}
+
+struct Characters: Decodable {
+    let name: String?
+    let status: String?
+    let species: String?
+    let gender: String?
+    let origin: Location?
+    let location: Location?
+    let image: String?
+    let episode: [String]?
+    let url: String?
+}
+
+struct Location: Decodable {
+    let name: String?
+    let url: String?
+}
+
+struct Episode: Decodable {
+    let name: String?
+    let date: String?
+    let episode: String?
+    let characters: [String]?
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case date = "air_date"
+        case episode = "episode"
+        case characters = "characters"
+    }
+    
+    var description: String {
+        """
+        Title: \(episode ?? "")
+        Date: \(date ?? "")
+        """
+    }
+}
+
+enum URLS: String {
+    case rickAndMortyAPI = "https://rickandmortyapi.com/api/character"
+}
